197bb437014ef911a4bf2019862ca9eab31482fe
Romain Jacob <romain.p.jacob@icloud.com>
2026-04-13 00:10:02 +0200

    Add random_state for reproducibility

--- iris_classifier.py
+++ iris_classifier.py
@@ -20,7 +20,7 @@ def load_data():
 def train_and_evaluate(iris):
     X = iris.data
     y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = KNeighborsClassifier()
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
